32aac9937dfbc283e8b37dbb2b600e5633665ce9
Sranjan4321 <shashi94ranjan@gmail.com>
2022-03-31 19:29:10 +0530

    initial commit

---
+++ components/navbar.js
@@ -0,0 +1,29 @@
+document.addEventListener("click", (e) => {
+    const isdropdownbutton = e.target.matches('[data-dropdown-button]');
+    
+    if (!isdropdownbutton && e.target.closest('[data-dropdown]') != null) return;
+    let currentdropdown;
+    if (isdropdownbutton) {
+        
+      currentdropdown = e.target.closest('[data-dropdown]');
+     
+      currentdropdown.classList.toggle("active");
+      currentdropdown.classList.toggle("indicate");
+    }
+    document.querySelectorAll("[data-dropdown].active").forEach((dropdowns) => {
+      if (dropdowns === currentdropdown)
+      {
+          console.log(currentdropdown)
+          return;
+      } 
+     
+      dropdowns.classList.remove("active");
+      dropdowns.classList.remove("indicate");
+    });
+  });
+  
+  let togglebutton=document.getElementsByClassName('toggle-button')[0]
+  let navbarlink=document.getElementsByClassName("remain")[0]
+  togglebutton.addEventListener("click",()=>{
+    navbarlink.classList.toggle('active')
+  })
---
+++ components/navbarhtml.js
@@ -0,0 +1,209 @@
+function navbar(){
+    return ` <div class="logo">
+    <!-- https://www.pluralsight.com/etc/clientlibs/pluralsight/main/images/global/header/PS_logo.png -->
+    <a href="#" class="comp_logo"></a>
+  </div>
+  <a href="#" class="toggle-button">
+    <span class="bar"></span>
+    <span class="bar"></span>
+    <span class="bar"></span>
+  </a>
+  <div class="remain">
+    <ul class="menu_main">
+      <li class="primary_link">
+        <div class="dropdown" data-dropdown>
+          <div class="link" style="color: white" data-dropdown-button>
+            Platform
+          </div>
+          <div class="dropdown_menu" >
+            <div class="platform_main">
+              <div class="first">
+               <p class="first_head">ways to upskill</p>
+               <ul class="first_lists">
+                 <li class="lists"><a href="#"><span><i class="fas fa-qrcode"></i></span>Course</a></li>
+                 <li class="lists"><a href="#"><span><i class="fas fa-map-marker-alt"></i></span>Skill assessments</a></li>
+                 <li class="lists"><a href="#"><span><i class="fas fa-cloud"></i></i></span>labs</a></li>
+                 <li class="lists"><a href="#"><span><i class="fas fa-hand-sparkles"></i></span>hands on learning</a></li>
+                 <li class="lists"><a href="#"><span ><i class="fas fa-stamp"></i></span>Certification prep</a></li>
+                 <li class="lists"><a href="#"><span><i class="fas fa-headset"></i></span>Live Class</a></li>
+                 <li class="lists"><a href="#"><span><i class="fab fa-buffer"></i></span>Team efficiency</a></li>
+               </ul>
+              </div>
+              <div class="second">
+                <div class="row1">
+                  <div class="tech">
+                    <p>skills for</p>
+                    <ul class="second_lists" style="list-style: none;">
+                    <li><a href="#">Software developement</a></li>
+                    <li><a href="#">IT ops</a></li>
+                    <li><a href="#"></a>info & Cyber Security</li>
+                    <li><a href="#">Clod computing</a></li>
+                    <li><a href="#">machine learning/AI</a></li>
+                    <li><a href="#">Data Professionals</a></li>
+                    </ul>
+                  </div>
+                  <div class="trending">
+                    <p>top trending paths</p>
+                    <div class="trend_item">
+                        <img src="https://pluralsight2.imgix.net/paths/images/group-policy-administration-ee0dacafe8.png?w=75" alt="microsoft" class="item_image">
+                        <div>microsoft Azure Deployment</div>
+                    </div>
+                    <div class="trend_item">
+                      <img src="https://pluralsight2.imgix.net/paths/images/r-programming-eb2e267a97.png?w=75" alt="microsoft" class="item_image">
+                      <div>Cleaning data with R</div>
+                  </div>
+                  <div class="trend_item">
+                    <img src="https://pluralsight2.imgix.net/paths/images/ruby-7e88ddd5f0.png?w=75" alt="microsoft" class="item_image">
+                    <div>Ruby Language Fundamentals</div>
+                </div>
+                  </div>
+                  <div class="other"><div class="trend_item">
+                    <img src="https://pluralsight2.imgix.net/paths/images/aws-certified-devops-engineer-25d912e3da.png?w=75" alt="microsoft" class="item_image">
+                    <div>AWS operations</div>
+                  </div>
+                   <div class="trend_item">
+                  <img src="https://pluralsight2.imgix.net/paths/images/python-7be70baaac.png?w=75" alt="microsoft" class="item_image">
+                  <div>Core Python</div>
+                  </div>
+                  </div>
+                </div>
+                <div class="row2">
+                  <span style="border-right: 1px solid gray;padding-right: 10px;margin-right: 10px">See Our Entire course libarary</span>
+                  <span>
+                  Check out the Technology Index</span></div>
+             
+              </div>
+             
+              
+            </div>
+          </div>
+          <button class="link" data-dropdown-button></button>
+        </div>
+      </li>
+
+      <li class="primary_link">
+        <div class="dropdown" data-dropdown>
+          <div class="link" style="color: white" data-dropdown-button>
+            product
+          </div>
+          <div class="dropdown_menu">
+            <!-- https://www.pluralsight.com/content/dam/pluralsight2/experiment-fragment/navigation/Skill_logo_white.png -->
+            <div class="product_main">
+              <div class="product left">
+              <a href="#"> <img src="https://www.pluralsight.com/content/dam/pluralsight2/experiment-fragment/navigation/Skill_logo_white.png" alt="">
+                <h3>Build tech skills to drive results</h3>
+              </a>
+               <hr class="product_hr">
+                <ul>
+                  <li><a href="#">What is Skills?</a></li>
+                  <li><a href="#">View plans</a></li>
+                </ul>
+              
+              </div>
+              <div class="product right">
+                <a href="#"> <img src="https://www.pluralsight.com/content/dam/pluralsight2/experiment-fragment/navigation/Flow_Logo_white.png" alt="">
+                  <h3>Build tech skills to drive results</h3>
+                </a>
+                 <hr class="product_hr">
+                  <ul>
+                    <li><a href="#">What is Flow?</a></li>
+                    <li><a href="#">View plans</a></li>
+                  </ul>
+              </div>
+            </div>
+
+          </div>
+          <button class="link" data-dropdown-button></button>
+        </div>
+      </li>
+      <li class="primary_link">
+        <div class="dropdown" data-dropdown>
+          <div class="link" style="color: white" data-dropdown-button>
+            Resources
+          </div>
+          <div class="dropdown_menu" >
+           <div class="resource_main">
+             <div class="left_resource">
+              <div class="left1">
+                <p>Learn</p>
+                <ul class="learn">
+                  <li><a href="#">Blog</a></li>
+                  <li><a href="#">Customer stories</a></li>
+                  <li><a href="#">Resource center</a></li>
+                  <li><a href="#">Guides</a></li>
+                  <li><a href="#">App download</a></li>
+                  <li><a href="#">Support</a></li>
+                </ul>
+              </div>
+              <div class="left2">
+                <p>connect</p>
+                <ul class="connect">
+                  <li><a href="#"> Events</a></li>
+                  <li> <a href="#">Webinars</a></li>
+                  <li> <a href="#">Podcast</a></li>
+                </ul>
+                <p>podcasts</p>
+                <ul class="podcasts">
+                  <li> <a href="#">All Hands on Tech</a></li>
+                  <li> <a href="#">Perspectives in Leadership</a></li>
+                 
+                </ul>
+              </div>
+             </div>
+             <div class="right_resource">
+             <h3>2022 Tech Forecast and<br/>
+              Build Better Blueprint</h3>
+              <h6>PREPARE FOR SHIFTS AND TRENDS IN THE INDUSTRY</h6>
+              <button class="resources_btn btn">Check it out</button>
+             </div>
+           </div>
+          </div>
+          <button class="link" data-dropdown-button></button>
+        </div>
+      </li>
+      <li class="primary_link"><a href="">ForInduvisuals</a></li>
+    </ul>
+    <ul
+      class="other_main"
+      style="display: flex; justify-content: space-between"
+    >
+      <li class="primary_link try">
+        <a class="tryfree" href="#">Try For Free</a>
+      </li>
+      <li class="primary_link">
+        <i class="fas fa-search"></i>
+      </li>
+      <li class="primary_link">
+        <i class="fas fa-phone-alt"></i>
+      </li>
+      <li class="primary_link sign">
+        <div class="dropdown" data-dropdown>
+          <div class="link" style="color: white" data-dropdown-button>
+            SignIn
+          </div>
+          <div class="dropdown_menu_login" style="color: black">
+          <div class="sign_main">
+            <div class="left_sign">
+              <a href="#">
+              <p>sighn in with</p>
+              <img src="https://www.pluralsight.com/content/dam/pluralsight2/experiment-fragment/navigation/Skill_logo_white.png" alt="">
+              </a>
+              </div>
+              <div class="right_sign">
+                <a href="#">
+                  <p>sighn in with</p>
+                  <img src="https://www.pluralsight.com/content/dam/pluralsight2/experiment-fragment/navigation/Flow_Logo_white.png" alt="">
+                  </a>
+              </div>
+             
+            </div>
+          </div>
+          </div>
+          <button class="link" data-dropdown-button></button>
+        </div>
+      </li>
+    </ul>
+  </div>`
+}
+
+export default navbar
